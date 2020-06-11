4b2eae39572cdd155e6ef6a5c81deec355bc65e7
siyam04 <galib.abdullah04@gmail.com>
2020-06-11 17:34:02 +0600

    basic layout added

---
+++ src/components/layouts/Master.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Master"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/routers/router.js
@@ -0,0 +1,30 @@
+// /*system*/
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+//
+// /*custom components*/
+// // import Home from '../components/pages/Home'
+// // import Blog from '../components/pages/Blog'
+// // import Posts from '../components/pages/Posts'
+//
+//
+//
+// /*inserting VueRouter to the Vue framework*/
+// Vue.use(VueRouter);
+//
+// /*array of paths*/
+// const routes = [
+//
+//     {path: '/', component: Home, name: 'Home'},
+//     {path: '/blog', component: Blog, name: 'Blog'},
+//     {path: '/posts/:id', component: Posts, name: 'Posts'},
+//
+// ];
+//
+// /*exporting Array of paths*/
+// export const router = new VueRouter({
+//     mode: 'history', //hides hash in url after localhost:8080
+//     routes: routes
+// });
+
+
